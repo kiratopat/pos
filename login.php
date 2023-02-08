075bbf78c73543fad83f3221957a9f3f0e1c4a18
kiratopat <kiratopat@protonmail.com>
2023-02-08 18:37:31 +0700

    clean login

--- login.php
+++ login.php
@@ -44,14 +44,6 @@
                     </div>
                 </form>
 
-
-
-                <p class="mb-1">
-                    <a href="forgot-password.html">I forgot my password</a>
-                </p>
-                <p class="mb-0">
-                    <a href="register.html" class="text-center">Register a new cashier</a>
-                </p>
             </div>
             <!-- /.card-body -->
         </div>
